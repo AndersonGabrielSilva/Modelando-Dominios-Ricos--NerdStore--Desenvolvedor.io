767fdef025f09fb4af49cbf44c27a5f8a67f054e
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2022-05-01 20:22:56 -0300

    Anotação: Interface de Marcação

--- src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
+++ src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -1,4 +1,9 @@
 ﻿namespace NerdStore.Core.DomainObjects
 {
+    /// <summary>
+    /// Interface de marcação - é uma boa pratica
+    /// <para>Serve para identificar se a classe é uma raiz de Agregação</para>
+    /// <para>Varios pacotes utilizam uma interface de marcação</para>
+    /// </summary>
     public interface IAggregateRoot { }
 }
